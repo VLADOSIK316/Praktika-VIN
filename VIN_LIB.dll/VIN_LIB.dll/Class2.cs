414e9c6f19d20394ecbcce7b33f9af7593347ae7
Pasus <Pausov1999@mail.ru>
2020-02-13 17:46:41 +0500

    Aer

---
+++ VIN_LIB.dll/VIN_LIB.dll/Class2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VIN_LIB.dll
+{
+    class Class2
+
+
+        Dictionary<string, string> COUN = new Dictionary<string, string>
+    {
+            {"A-H"="Африка"},{"J-R"="Азия"},{"S-Z"="Европа"},{"1-5"="Северная Америка"},{"6-7"="Океания"},{"8-9"="Южная Америка"}
+
+    };
+    Dictionary<string, string> cod = new Dictionary<string, string>
+    {
+        {"AA-AH"=  }
+    }
+    
